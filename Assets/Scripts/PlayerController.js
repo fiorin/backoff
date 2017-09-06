8aa915ed882a29dcc32f5733cf391c568d088232
fiorin <luciano@fior.in>
2017-09-06 15:19:02 -0300

    Started backoff project

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,87 @@
+﻿var maxSpeed : float;
+var facingRight : boolean = false;
+var rb2D : Rigidbody2D;
+var animator : Animator;
+var skills : Array = new Array();
+var gamepad : XboxCtrlrInput.XCI;
+var castOrigin : Transform;
+var target : Transform;
+var sprite : Transform;
+
+function Start() {
+	animator = GetComponent.<Animator>();
+	rb2D = GetComponent.<Rigidbody2D>();
+	skills.push(GetComponent.<SkillFireball>() as SkillFireball);
+	skills.push(GetComponent.<SkillFireball>() as SkillFireball);
+	castOrigin = transform.Find("CastOrigin");
+	target = castOrigin.Find("Target");
+	sprite = transform.Find("Character");
+	maxSpeed = 5;
+}
+
+function Update() {
+	MoveManager();
+	AimManager();
+	ButtonManager();
+}
+
+function MoveManager(){
+	var x = Input.GetAxis("Horizontal");
+	var y = Input.GetAxis("Vertical");
+	//animator.SetFloat("speed",Mathf.Abs(x));
+	rb2D.velocity.x = x * maxSpeed;
+	rb2D.velocity.y = y * maxSpeed;
+	if(x > 0 && !facingRight || x < 0 && facingRight)
+		faceInverse();
+	return;
+}
+
+function AimManager(){
+	var rx = gamepad.GetAxis(XboxCtrlrInput.XboxAxis.RightStickX);
+	var ry = gamepad.GetAxis(XboxCtrlrInput.XboxAxis.RightStickY);
+	var v1 = Vector3(rx,ry,0);
+	v1.Normalize();
+	if(v1 == Vector3.zero){
+		target.GetComponent.<Renderer>().enabled = false;
+	}else{
+		target.GetComponent.<Renderer>().enabled = true;
+	}
+	target.transform.localPosition = v1 * 1.5;
+	return;
+}
+
+function ButtonManager(){
+	if(gamepad.GetAxisRaw(XboxCtrlrInput.XboxAxis.RightTrigger)){
+		skills[0].Cast();
+	}
+	if(gamepad.GetAxisRaw(XboxCtrlrInput.XboxAxis.LeftTrigger)){
+		skills[1].Cast();
+	}
+	if(gamepad.GetButtonDown(XboxCtrlrInput.XboxButton.A)){
+		//
+	}
+	if(gamepad.GetButtonDown(XboxCtrlrInput.XboxButton.B)){
+		//
+	}
+	if(gamepad.GetButtonDown(XboxCtrlrInput.XboxButton.Y)){
+		//
+	}
+	if(gamepad.GetButtonDown(XboxCtrlrInput.XboxButton.X)){
+		//
+	}
+	return;
+}
+
+function faceInverse(){
+	var scale : Vector3;
+	scale = sprite.transform.localScale;
+	scale.x *= -1;
+	facingRight = !facingRight;
+	sprite.transform.localScale = scale;
+	return;
+}
+
+
+function OnCollisionEnter2D(other : Collision2D){
+	Debug.Log('teste');
+}
---
+++ Assets/Scripts/SkillFireballBehaviour.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var rb2D : Rigidbody2D;
+var facingRight : boolean;
+var angle : float;
+var direction : Vector2;
+var origin : Vector2;
+var iniciar : boolean;
+var projectileSpeed : float;
+
+function Start () {
+	rb2D = GetComponent.<Rigidbody2D>();
+	direction = transform.position - origin;
+	direction.Normalize();
+	projectileSpeed = 8;
+	rb2D.velocity = direction * projectileSpeed;
+	var moveDirection = rb2D.velocity;
+    if (moveDirection != Vector2.zero){
+        var angle = Mathf.Atan2(-moveDirection.y, -moveDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
+
+function Update () {
+	
+}
+
+function OnCollisionEnter2D(other : Collision2D){
+	//print("Points colliding: " + other.contacts.Length);
+    print("First point that collided: " + other.contacts[0].point);
+    Debug.Log(other.contacts.Length);
+    Destroy(this.gameObject);
+}
